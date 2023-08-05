a30bbf2b9688cbcbab3dc4df71ae68fe5abacd67
Clarissa Yeo <clarissayeo94@gmail.com>
2023-08-05 23:00:54 +0800

    Here is my commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+  end
+
+  def update
+    @article =Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  # Read All
+  get "articles", to: "articles#index"
+
+  # Create
+  get "articles/new", to: "articles#new", as: :new_article
+
+  post "articles", to: "articles#create"
+
+  get "articles/:id", to: "articles#show", as: :article
+
+  get "articles/:id/edit", to: "articles#edit", as: :edit
+
+  delete "articles/:id", to: "articles#destroy", as: :delete
+
+  patch "articles/:id", to: "articles#update"
 end
